e3b8f3e56b621fe2085d0746e2353fbff1019e8c
espoira <irlys91@mail.ru>
2021-06-21 21:49:05 +0300

    вносит изменения в файл javascript

--- js/browser-differences.js
+++ js/browser-differences.js
@@ -1,4 +1,10 @@
 ﻿var user = detect.parse(navigator.userAgent);
+
 	if (user.browser.family == "Firefox") {
-		document.getElementById('div-grad').style.width = "95px";
+		document.getElementById('div-grad').style.width = "105px";
+	};
+
+	if (user.browser.family == "Chrome") {
+		document.getElementById('dates').style.position = "relative";
+		document.getElementById('dates').style.top = "30px";
 	};
